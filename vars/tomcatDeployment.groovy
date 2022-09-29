dba9ff1c01e52443d9204fa08a071ae12e014b4c
nikesh v <54545457+Nikesh6920@users.noreply.github.com>
2022-09-29 17:35:49 +0530

    Update tomcatDeployment.groovy

--- vars/tomcatDeployment.groovy
+++ vars/tomcatDeployment.groovy
@@ -2,9 +2,9 @@ def call(String sshagentid, String serverid) {
     sh "echo Hello ${serverid} ."
     sh "echo Hello ${sshagentid} ."
     sshagent(['${sshagentid}']) {
-    sh 'ssh -o StrictHostKeyChecking=no '[ec2-user@]${serverid}' sudo /opt/apache-tomcat-8.5.82/bin/./shutdown.sh'
-    sh 'ssh -o StrictHostKeyChecking=no '[ec2-user@]${serverid}' sudo rm -f /opt/apache-tomcat-8.5.82/webapps/webapp.war rm -fr /opt/apache-tomcat-8.5.82/webapps/webapp'
-    sh 'scp -o StrictHostKeyChecking=no /var/lib/jenkins/workspace/ScriptedPipeline/target/webapp.war '[ec2-user@]${serverid}':/opt/apache-tomcat-8.5.82/webapps/'
-    sh 'ssh -o StrictHostKeyChecking=no '[ec2-user@]${serverid}' sudo /opt/apache-tomcat-8.5.82/bin/./startup.sh'
+    sh 'ssh -o StrictHostKeyChecking=no 'ec2-user@${serverid}' sudo /opt/apache-tomcat-8.5.82/bin/./shutdown.sh'
+    sh 'ssh -o StrictHostKeyChecking=no 'ec2-user@${serverid}' sudo rm -f /opt/apache-tomcat-8.5.82/webapps/webapp.war rm -fr /opt/apache-tomcat-8.5.82/webapps/webapp'
+    sh 'scp -o StrictHostKeyChecking=no /var/lib/jenkins/workspace/ScriptedPipeline/target/webapp.war 'ec2-user@${serverid}':/opt/apache-tomcat-8.5.82/webapps/'
+    sh 'ssh -o StrictHostKeyChecking=no 'ec2-user@${serverid}' sudo /opt/apache-tomcat-8.5.82/bin/./startup.sh'
     }
 }
