651bad6402af3449f1d1480c83febb94228de155
nikesh v <54545457+Nikesh6920@users.noreply.github.com>
2022-09-29 16:09:35 +0530

    Update tomcatDeployment.groovy

--- vars/tomcatDeployment.groovy
+++ vars/tomcatDeployment.groovy
@@ -1,9 +1,9 @@
 def call(Map stageParams) {
-    sh "echo Hello ${stageParams.serverid} ."
-    sshagent(['${stageParams.ssh-agent}']) {
-    sh 'ssh -o StrictHostKeyChecking=no ec2-user@${stageParams.serverid} sudo /opt/apache-tomcat-8.5.82/bin/./shutdown.sh'
-    sh 'ssh -o StrictHostKeyChecking=no ec2-user@${stageParams.serverid} sudo rm -f /opt/apache-tomcat-8.5.82/webapps/webapp.war rm -fr /opt/apache-tomcat-8.5.82/webapps/webapp'
-    sh 'scp -o StrictHostKeyChecking=no /var/lib/jenkins/workspace/ScriptedPipeline/target/webapp.war ec2-user@${stageParams.serverid}:/opt/apache-tomcat-8.5.82/webapps/'
-    sh 'ssh -o StrictHostKeyChecking=no ec2-user@${stageParams.serverid} sudo /opt/apache-tomcat-8.5.82/bin/./startup.sh'
+    sh "echo Hello ${serverid: stageParams.serverid} ."
+    sshagent(['${ssh-agent: stageParams.ssh-agent}']) {
+    sh 'ssh -o StrictHostKeyChecking=no ec2-user@${serverid: stageParams.serverid} sudo /opt/apache-tomcat-8.5.82/bin/./shutdown.sh'
+    sh 'ssh -o StrictHostKeyChecking=no ec2-user@${serverid: stageParams.serverid} sudo rm -f /opt/apache-tomcat-8.5.82/webapps/webapp.war rm -fr /opt/apache-tomcat-8.5.82/webapps/webapp'
+    sh 'scp -o StrictHostKeyChecking=no /var/lib/jenkins/workspace/ScriptedPipeline/target/webapp.war ec2-user@${serverid: stageParams.serverid}:/opt/apache-tomcat-8.5.82/webapps/'
+    sh 'ssh -o StrictHostKeyChecking=no ec2-user@${serverid: stageParams.serverid} sudo /opt/apache-tomcat-8.5.82/bin/./startup.sh'
     }
 }
