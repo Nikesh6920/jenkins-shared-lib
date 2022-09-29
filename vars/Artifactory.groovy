b15e1750b1bf4201168a1fa44ccfc6a6b1246ae2
nikesh v <54545457+Nikesh6920@users.noreply.github.com>
2022-09-29 12:09:10 +0530

    Create Artifactory.groovy

---
+++ vars/Artifactory.groovy
@@ -0,0 +1,21 @@
+def upload(String serverid) {
+    sh "echo Uploading..."
+    rtUpload (
+        serverId: "${serverid}" ,
+                    spec: """{
+                            "files": [
+                                    {
+                                        "pattern": "*.war",
+                                        "target": "libs-snapshot-local"
+                                    }
+                                ]
+                            }"""
+                )
+}
+
+def publish(String serverid) {
+    sh "echo Publishing Artifact Info..."
+    rtPublishBuildInfo (
+                    serverId: "${serverid}"
+                )
+}
